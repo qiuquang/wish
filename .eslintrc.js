5355afa1e4a95d716fb73bc6c37ed3f3965afbd3
qiuquan <qiuquan@qdcares.cn>
2020-09-28 20:16:09 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        'vue/no-parsing-error': [2, { "x-invalid-end-tag": false }],
+        'allowConstructorFlags': 0
+    }
+};
